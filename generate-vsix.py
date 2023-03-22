b2028457ce06a3fd867a2fa64f5f98cf946bc250
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-22 02:04:20 -0300

    2.0.2: reordered and changed f/r colors

--- generate-vsix.py
+++ generate-vsix.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 
-def zipfolder(filename, target_dir):
+def zipfolder(version):
     """
     Zip the contents of an entire folder (with that folder included
     in the archive). Empty subfolders will be included in the archive
@@ -11,29 +11,29 @@ def zipfolder(filename, target_dir):
 
     Parameters
     ----------
-    filename : str
-        The name of the zip file.
-    target_dir : str
-        The path of the folder to be zipped.
-
-    Returns
-    -------
-    None
-        Nothing is returned.
+    version : str
+        The version number of the theme to be zipped.
 
     Examples
     --------
-    >>> zipfolder("myzipfile", "myfolder")
+    >>> zipfolder("2.0.2")
 
     """
     extension = "vsix"
-    zipobj = zipfile.ZipFile(f"{filename}.{extension}", "w", zipfile.ZIP_DEFLATED)
-    rootlen = len(target_dir) + 1
-    for base, _dirs, files in os.walk(target_dir):
+    root_folder = os.getcwd()
+    old_versions_folder = os.path.join(root_folder, "old_versions")
+    os.makedirs(old_versions_folder, exist_ok=True)
+    vsix_files = [f for f in os.listdir(root_folder) if f.endswith(
+        '.vsix') and f != f"darkam-theme-v{version}.{extension}"]
+    for old_vsix in vsix_files:
+        os.rename(os.path.join(root_folder, old_vsix), os.path.join(old_versions_folder, old_vsix))
+    zipobj = zipfile.ZipFile(f"darkam-theme-v{version}.{extension}", "w", zipfile.ZIP_DEFLATED)
+    rootlen = len("./src") + 1
+    for base, _dirs, files in os.walk("./src"):
         for file in files:
             fn = os.path.join(base, file)
             zipobj.write(fn, fn[rootlen:])
-    return sys.exit()
+    sys.exit()
 
 
-zipfolder("darkam-theme-v2.0.1", "./src")
+zipfolder("2.0.2")
